984ffe02291e360c2405e73d841ccd2c5ffd0d86
Sadman9061 <sadmansakib.dev@gmail.com>
2023-10-21 20:05:18 +0600

    Design the login and sign-up interfaces, preparing for user authentication.

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import Swal from "sweetalert2";
-// import Swal f
+// imp
 
 const MyCart = () => {
     const cartsData = useLoaderData();
